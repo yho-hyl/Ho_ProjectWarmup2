02cf3f9252975039b6dadd0c6825dbadf01595f7
Yusuke Ho <yhowtcc@my.waketech.edu>
2026-03-05 13:57:50 -0500

    ;

---
+++ script.js
@@ -0,0 +1,30 @@
+const bedroom = {
+    image: "image/bedroom.jpg",
+    text: "old cloth and damaged wall",
+    choices: "Go out the room"
+}
+
+const enterence = {
+    image: "image/enterence.jpg",
+    text: "old cloth and damaged wall",
+    choices: "Go into the room"
+}
+
+const hallway = {
+    image: "image/hallway.jpg",
+    text: "old cloth and damaged wall",
+    choices: "Go down the hallway"
+}
+
+const stairs = {
+    image: "image/stairs.jpg",
+    text: "old cloth and damaged wall",
+    choices: "Go up the starirs"
+}
+
+let storyNodes = [bedroom, enterence, hallway, stairs];
+console.log(bedroom.image);
+
+function displayNode(){
+    
+}
